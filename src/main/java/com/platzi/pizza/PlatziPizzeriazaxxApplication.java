d8ed27f2c1a127f04d3b6387c396fd147070b155
Franco Alva <franco99alva@gmail.com>
2023-06-08 10:49:49 -0500

    Se inició el projecto y se terminaron las entidades y sus relaciones

---
+++ src/main/java/com/platzi/pizza/PlatziPizzeriazaxxApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziPizzeriazaxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziPizzeriazaxxApplication.class, args);
+	}
+
+}
